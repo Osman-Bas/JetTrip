9d02b1c5a3c6d853a4d7886b0be1fe263afcb730
Osman Baş <obas1869@gmail.com>
2025-04-27 02:53:09 +0300

    Eksik klasör eklendi

---
+++ JetTrip/App/JetTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  JetTripApp.swift
+//  JetTrip
+//
+//  Created by Osman Baş on 27.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct JetTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
