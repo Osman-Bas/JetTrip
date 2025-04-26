22dee20c7731f3cddc751d792c31398aef244c0c
Osman Baş <obas1869@gmail.com>
2025-04-27 02:31:39 +0300

    Initial Commit

---
+++ JetTripTests/JetTripTests.swift
@@ -0,0 +1,17 @@
+//
+//  JetTripTests.swift
+//  JetTripTests
+//
+//  Created by Osman Baş on 27.04.2025.
+//
+
+import Testing
+@testable import JetTrip
+
+struct JetTripTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
