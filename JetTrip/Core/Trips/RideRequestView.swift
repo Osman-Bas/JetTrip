e84146311ee79f5a13666647a005bef69b34f2da
Osman Baş <obas1869@gmail.com>
2025-05-01 12:31:42 +0300

    Ride request view'i tasarlandı.

--- JetTrip/Core/Trips/RideRequestView.swift
+++ JetTrip/Core/Trips/RideRequestView.swift
@@ -9,7 +9,104 @@ import SwiftUI
 
 struct RideRequestView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Capsule()
+                .foregroundColor(Color(.systemGray5))
+                .frame(width: 48, height: 6)
+            
+            // trip info view
+            HStack{
+                //indicator View
+                VStack{
+                    Circle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 8, height: 8)
+                    
+                    Rectangle()
+                        .fill(Color(.systemGray3))
+                        .frame(width: 1, height: 32)
+                    
+                    Rectangle()
+                        .fill(.black)
+                        .frame(width: 8, height: 8)
+                }
+                VStack(alignment: .leading, spacing: 24){
+                    HStack{
+                        Text("Konumunuz")
+                            .font(.system(size: 16, weight: .semibold))
+                            .foregroundColor(.gray)
+                        
+                        Spacer()
+                        
+                        Text("1.30 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                    .padding(.bottom, 10)
+                    
+                    HStack{
+                        Text("Starbucks Coffee")
+                            .font(.system(size: 16,weight: .semibold))
+                            
+                        
+                        Spacer()
+                        
+                        Text("1.44 PM")
+                            .font(.system(size: 14, weight: .semibold))
+                            .foregroundColor(.gray)
+                    }
+                }
+                .padding(.leading, 8)
+            }
+            .padding()
+            
+            Divider()
+            
+            //ride type selection view
+            
+            Text("ÖNERİLEN SEFERLER")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+                .padding()
+                .foregroundColor(.gray)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal){
+                HStack(spacing:12){
+                    ForEach(0..<3, id: \.self){_ in
+                        VStack(alignment: .leading){
+                            Image("uber-x")
+                                .resizable()
+                                .scaledToFit( )
+                            
+                            VStack(spacing: 4) {
+                                Text("Uber-X")
+                                    .font(.system(size: 14, weight: .semibold))
+                            
+                            
+                            Text("255.90₺")
+                                .font(.system(size: 14, weight: .semibold))
+                            }
+                            .padding(8)
+                        }
+                        
+                        .frame(width: 112, height: 140)
+                        .background(Color(.systemGroupedBackground))
+                        .cornerRadius(10)
+                    }
+                }
+            }
+            .padding(.horizontal)
+            
+            //payment option view
+            
+            HStack(spacing:12){
+                Text("")
+            }
+            
+            //request ride button
+            
+        }
     }
 }
 
