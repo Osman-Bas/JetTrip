f8247e3b9e39861b01ab4cf2af12ef8a502d48dd
Osman Baş <obas1869@gmail.com>
2025-05-01 15:15:28 +0300

    Oluşan buglar fixlendi.

--- JetTrip/Core/Home/Views/UberMapViewRepresentable.swift
+++ JetTrip/Core/Home/Views/UberMapViewRepresentable.swift
@@ -94,9 +94,8 @@ extension UberMapViewRepresentable{
             
             let anno = MKPointAnnotation()
             anno.coordinate = coordinate
-            self.parent.mapView.addAnnotation(anno)
-            self.parent.mapView.selectAnnotation(anno, animated: true)
-            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+            parent.mapView.addAnnotation(anno)
+            parent.mapView.selectAnnotation(anno, animated: true)
         }
         
         func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D){
@@ -104,6 +103,8 @@ extension UberMapViewRepresentable{
             getDestinationRoute(from: userLocationCoordinate,
                                 to: coordinate){route in
                 self.parent.mapView.addOverlay(route.polyline)
+                let rect = self.parent.mapView.mapRectThatFits(route.polyline.boundingMapRect,edgePadding: .init(top: 64, left: 32, bottom: 500, right: 32))
+                self.parent.mapView.setRegion(MKCoordinateRegion(rect), animated: true)
             }
         }
         
