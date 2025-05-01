361b837af96e41184aa22ea9709685bc665f8025
Osman Baş <obas1869@gmail.com>
2025-05-01 12:29:32 +0300

    Assets-1

---
+++ JetTrip/Core/Trips/RideRequestView.swift
@@ -0,0 +1,18 @@
+//
+//  RideRequestView.swift
+//  JetTrip
+//
+//  Created by Osman Baş on 30.04.2025.
+//
+
+import SwiftUI
+
+struct RideRequestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RideRequestView()
+}
