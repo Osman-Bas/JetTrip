99f065a0a3528d271735fab6dc5746c2d917fa23
Osman Baş <obas1869@gmail.com>
2025-05-06 23:27:25 +0300

    View'deki componentler ayarlandı.

--- JetTrip/Core/Trips/RideRequestView.swift
+++ JetTrip/Core/Trips/RideRequestView.swift
@@ -41,20 +41,22 @@ struct RideRequestView: View {
                         
                         Spacer()
                         
-                        Text("1.30 PM")
+                        Text(locationViewModel.pickupTime ?? "")
                             .font(.system(size: 14, weight: .semibold))
                             .foregroundColor(.gray)
                     }
                     .padding(.bottom, 10)
                     
                     HStack{
-                        Text("Starbucks Coffee")
-                            .font(.system(size: 16,weight: .semibold))
+                        if let location = locationViewModel.selectedUberLocation{
+                            Text(location.title)
+                                .font(.system(size: 16,weight: .semibold))
+                        }
                             
                         
                         Spacer()
                         
-                        Text("1.44 PM")
+                        Text(locationViewModel.dropOffTime ?? "")
                             .font(.system(size: 14, weight: .semibold))
                             .foregroundColor(.gray)
                     }
