e96805792a47b621a5552d213298b30f4066c2fe
Osman Baş <obas1869@gmail.com>
2025-05-11 23:09:38 +0300

    Fixing bugs.

--- JetTrip/Core/Home/Views/MapViewActionButton.swift
+++ JetTrip/Core/Home/Views/MapViewActionButton.swift
@@ -30,7 +30,7 @@ actionForState(mapState)            }
     func actionForState(_ state: MapViewState){
         switch state {
         case .noInput:
-            print("Debug: no input action triggered:")
+            print("Debug: no input action triggered - no action to perform:")
         case .searchingForLocation:
             mapState = .noInput
         case .locationSelected, .polylineAdded:
