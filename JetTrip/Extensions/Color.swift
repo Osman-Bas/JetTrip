12d939160a133b6b8270bb5d4db54ccb65476312
Osman Baş <obas1869@gmail.com>
2025-05-13 22:51:11 +0300

    Dark mode için renk paletleri oluşturuldu.

--- JetTrip/Extensions/Color.swift
+++ JetTrip/Extensions/Color.swift
@@ -5,4 +5,14 @@
 //  Created by Osman Baş on 12.05.2025.
 //
 
-import Foundation
+import SwiftUI
+extension Color {
+    static let theme = ColorTheme()
+}
+
+
+struct ColorTheme {
+    let backgroundColor = Color("BackgroundColor")
+    let secondaryBackgroundColor = Color("SecondaryBackgroundColor")
+    let primaryTextColor = Color("PrimaryTextColor")
+}
